cbcf2e1536eae44c509c3b96b8c73310c677582a
Marek Truska <truskamarek@centrum.sk>
2022-11-15 20:58:05 +0100

    Added basic html/css skeleton and defined basic operators functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function addition(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function division(n1, n2) {
+  return n1 / n2
+}
+
