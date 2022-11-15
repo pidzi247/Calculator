6625051ebb1a82a7a2bfb84b9ad040c13f73e6d3
Marek Truska <truskamarek@centrum.sk>
2022-11-15 22:29:07 +0100

    Added operate function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Operator functions
 function addition(n1, n2) {
   return n1 + n2;
 }
@@ -14,3 +15,13 @@ function division(n1, n2) {
   return n1 / n2
 }
 
+//Operate function that takes one of the operator functions above and 
+//numbers as arguments
+
+function operate(operator, n1, n2) {
+  switch(operator) {
+    case "+":
+      return addition(n1, n2);
+      break;
+  }
+}
