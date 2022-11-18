35e0b19c864fc6ed43611871deea028ea36628ed
Marek Truska <truskamarek@centrum.sk>
2022-11-18 22:40:07 +0100

    Added operators work and properly show the result on display

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let display = document.getElementById('display');
 let numbers = document.querySelectorAll('.num');
+let operators = document.querySelectorAll('.operator');
 
-let displayValue = 0;
-
+let storedValue = null;
+let storedOperator = null;
 
 
 //Operator functions
@@ -19,7 +20,7 @@ function multiply(n1, n2) {
 }
 
 function division(n1, n2) {
-  return n1 / n2
+  return n1 / n2;
 }
 
 //Operate function that takes one of the operator functions above and 
@@ -30,14 +31,35 @@ function operate(operator, n1, n2) {
     case "+":
       return addition(n1, n2);
       break;
+    case "-":
+      return subtract(n1, n2);
+      break;
+    case "*":
+      return multiply(n1, n2);
+      break;
+    case "/":
+      return division(n1, n2);
+      break;
   }
 }
 
 numbers.forEach(num => {
-  num.addEventListener('click', updateDisplay);
+    num.addEventListener('click', updateDisplay);
 });
 
+operators.forEach(operator => {
+  operator.addEventListener('click', storeValue)
+})
+
 function updateDisplay(e) {
-  display.textContent = e.target.textContent;
-  displayValue = e.target.textContent;
+  if(storedValue !== null) {
+    display.textContent = operate(storedOperator, parseInt(storedValue), parseInt(e.target.textContent));
+  } else {
+    display.textContent = e.target.textContent;
+  }
+}
+
+function storeValue(e) {
+  storedValue = display.textContent;
+  storedOperator = e.target.textContent;
 }
