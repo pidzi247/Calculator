d6c1c6433c95f41203eebc1d4530099b706bd9db
Marek Truska <truskamarek@centrum.sk>
2022-11-20 22:59:31 +0100

    Fixed indentations and added descriptive comments

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/////////////////////////////////////////////////
+//VARIABLES SECTION!!!
+/////////////////////////////////////////////////
+
+//DOM variables to access proper elements
 const display = document.getElementById('display');
 const upperNum = document.getElementById('upper-number');
 const lowerNum = document.getElementById('lower-number');
@@ -7,11 +12,34 @@ const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
 const percent = document.getElementById('percentage')
 
+//Variables to hold data for calculation and clearing data
 let storedValue = 0;
 let storedOperator = null;
 let equalSignClicked = false;
 let evaluation = null;
 
+/////////////////////////////////////////////////
+//EVENT LISTENER SECTION!!!
+/////////////////////////////////////////////////
+
+//Event listeners for numbers, operator and special signs buttons
+numbers.forEach(num => {
+    num.addEventListener('click', updateDisplay);
+});
+
+operators.forEach(operator => {
+  operator.addEventListener('click', operate);
+});
+
+equals.addEventListener('click', result);
+
+clear.addEventListener('click', reset);
+
+percent.addEventListener('click', percentage);
+
+/////////////////////////////////////////////////
+//FUNCTION SECTION!!!
+/////////////////////////////////////////////////
 
 //Operator functions
 function addition(n1, n2) {
@@ -32,7 +60,6 @@ function division(n1, n2) {
 
 //Operate function that takes one of the operator functions above and 
 //numbers as arguments
-
 function calculate(operator, n1, n2) {
   switch(operator) {
     case "+":
@@ -50,21 +77,8 @@ function calculate(operator, n1, n2) {
   }
 }
 
-numbers.forEach(num => {
-    num.addEventListener('click', updateDisplay);
-});
 
-operators.forEach(operator => {
-  operator.addEventListener('click', operate);
-});
-
-equals.addEventListener('click', result);
-
-clear.addEventListener('click', reset);
-
-percent.addEventListener('click', percentage);
-
-
+//Function which updates proper display sections with their respective user inputs
 function updateDisplay(e) {
   if(lowerNum.textContent === "0") {
     lowerNum.innerHTML = "";
@@ -78,6 +92,7 @@ function updateDisplay(e) {
   
 }
 
+//Handles actual logic when and how to call operate function
 function operate(e) {
   if(equalSignClicked) {
     storedOperator = null;
@@ -97,6 +112,7 @@ function operate(e) {
   equalSignClicked = false;
 }
 
+//Clears all the necessary data for user to start over fresh and clean
 function reset(e) {
   upperNum.innerHTML = "";
   lowerNum.textContent = "0";
@@ -105,6 +121,7 @@ function reset(e) {
   evaluation = null;
 }
 
+//Handles logic when user hits 'equal' sign and provides visual clue when its hit
 function result(e) {
   upperNum.textContent = `${storedValue} ${storedOperator} ${lowerNum.textContent} ${e.target.textContent}`;
   evaluation = calculate(storedOperator, storedValue, parseInt(lowerNum.textContent))
