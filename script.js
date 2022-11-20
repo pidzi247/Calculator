a0c9e174da883a4ede2f42d8fdb23e1570659486
Marek Truska <truskamarek@centrum.sk>
2022-11-20 22:23:10 +0100

    Added equals button

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let display = document.getElementById('display');
-let numbers = document.querySelectorAll('.num');
-let operators = document.querySelectorAll('.operator');
+const display = document.getElementById('display');
+const upperNum = document.getElementById('upper-number');
+const lowerNum = document.getElementById('lower-number');
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const equals = document.getElementById('equals');
+const clear = document.getElementById('clear');
+const percent = document.getElementById('percentage')
 
-let storedValue = null;
+let storedValue = 0;
 let storedOperator = null;
+let equalSignClicked = false;
+let evaluation = null;
 
 
 //Operator functions
@@ -26,7 +33,7 @@ function division(n1, n2) {
 //Operate function that takes one of the operator functions above and 
 //numbers as arguments
 
-function operate(operator, n1, n2) {
+function calculate(operator, n1, n2) {
   switch(operator) {
     case "+":
       return addition(n1, n2);
@@ -48,18 +55,63 @@ numbers.forEach(num => {
 });
 
 operators.forEach(operator => {
-  operator.addEventListener('click', storeValue)
-})
+  operator.addEventListener('click', operate);
+});
+
+equals.addEventListener('click', result);
+
+clear.addEventListener('click', reset);
+
+percent.addEventListener('click', percentage);
+
 
 function updateDisplay(e) {
-  if(storedValue !== null) {
-    display.textContent = operate(storedOperator, parseInt(storedValue), parseInt(e.target.textContent));
+  if(lowerNum.textContent === "0") {
+    lowerNum.innerHTML = "";
+    lowerNum.textContent += e.target.textContent;
   } else {
-    display.textContent = e.target.textContent;
+    if(parseInt(lowerNum.textContent) === storedValue) {
+      lowerNum.textContent = "";
+    }
+    lowerNum.textContent += e.target.textContent;
   }
+  
 }
 
-function storeValue(e) {
-  storedValue = display.textContent;
-  storedOperator = e.target.textContent;
+function operate(e) {
+  if(equalSignClicked) {
+    storedOperator = null;
+  }
+  if(!storedOperator) {
+    storedValue = parseInt(lowerNum.textContent);
+    storedOperator = e.target.textContent;
+    upperNum.textContent = `${storedValue} ${storedOperator}`;  
+  } else {
+    evaluation = calculate(storedOperator, storedValue, parseInt(lowerNum.textContent))
+    upperNum.textContent = `${evaluation} ${storedOperator}`;
+    lowerNum.textContent = evaluation;
+    storedValue = evaluation;
+    storedOperator = e.target.textContent;
+    upperNum.textContent = `${storedValue} ${storedOperator}`;
+  }
+  equalSignClicked = false;
+}
+
+function reset(e) {
+  upperNum.innerHTML = "";
+  lowerNum.textContent = "0";
+  storedOperator = null;
+  storedValue = null;
+  evaluation = null;
+}
+
+function result(e) {
+  upperNum.textContent = `${storedValue} ${storedOperator} ${lowerNum.textContent} ${e.target.textContent}`;
+  evaluation = calculate(storedOperator, storedValue, parseInt(lowerNum.textContent))
+  lowerNum.textContent = evaluation;
+  equalSignClicked = true;
+}
+
+function percentage(e) {
+  storedValue /= 100;
 }
